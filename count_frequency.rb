eca872e9f3d62990ea0f95d5f389c9827ba3c3c5
Amelia Fannin <ameliafannin@gmail.com>
2015-02-14 14:22:16 -0500

    added spamminess percent calculations

--- count_frequency.rb
+++ count_frequency.rb
@@ -1,25 +1,41 @@
 
-def get_sample
+def get_spam_sample
   input = []
+  @spam_size = 0
 
   Dir.foreach("#{Dir.pwd}/spam/") do |name|
     next if name == '.' or name == '..' or name == 'cmds'
-    # puts name
 
     File.open "#{Dir.pwd}/spam/#{name}", "r" do |f|
-      f.each_line do |l|
-        input << l
-      end
+      f.each_line { |l| input << l }
     end
+    @spam_size += 1
   end
+  input
+end
 
-  return input
+def get_ham_sample
+  input = []
+  @ham_size = 0
+
+  Dir.foreach("#{Dir.pwd}/easy_ham/") do |name|
+    next if name == '.' or name == '..' or name == 'cmds'
+
+    File.open "#{Dir.pwd}/easy_ham/#{name}", "r" do |f|
+      f.each_line { |l| input << l }
+    end
+    @ham_size += 1
+  end
+  
+  input
 end
 
 def count_frequency
-  words = {}
+  stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
+  @spam = {}
+  @ham = {}
 
-  get_sample.each do |s|
+  get_spam_sample.each do |s|
     # removes non UTF-8 characters
     s.force_encoding("iso-8859-1").gsub!(/\W/, ' ')
 
@@ -31,20 +47,58 @@ def count_frequency
 
 
     s.split(' ').each do |w|
-
-      if w.length > 3
-        if words.has_key?(w)
-          words[w] += 1
-        else 
-          words[w] = 1
-        end
+      next if w.length <= 3 || w.length > 15 || stopwords.include?(w)
+      
+      if @spam.has_key?(w)
+        @spam[w] += 1
+      else 
+        @spam[w] = 1
       end
+
     end
   end
 
-  words
+  get_ham_sample.each do |s|
+    # removes non UTF-8 characters
+    s.force_encoding("iso-8859-1").gsub!(/\W/, ' ')
+
+    # s.gsub!(/^(((ht|f)tp(s?))\://)?((([a-zA-Z0-9_\-]{2,}\.)+[a-zA-Z]{2,})|((?:(?:25[0-5]|2[0-4]\d|[01]\d\d|\d?\d)(?(\.?\d)\.)){4}))(:[a-zA-Z0-9]+)?(/[a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~]*)?$)/, '')
+
+    # removes most email addresses
+    s.gsub!(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, ' ')
+
+    # removes html tags, punctuation, numbers
+    s.gsub!(/[ <.*> [[:punct:]] [[:digit:]] ]/, ' ')
+
+    s.split(' ').each do |w|
+      next if w.length <= 3 || w.length > 15 || stopwords.include?(w)
+      
+      if @ham.has_key?(w)
+        @ham[w] += 1
+      else 
+        @ham[w] = 1
+      end
+
+    end
+  end
 end
 
-puts count_frequency
+def calc_spamminess(word)
+  count_frequency
+  total_sample = (@spam_size + @ham_size).to_f
+  spam = @spam[word]
+  total_word = @ham[word] + @spam[word]
+  # puts "#{word} occurs in Spam: #{@spam[word]} of #{@spam_size}"
+  # puts "#{word} occurs in Ham: #{@ham[word]} of #{@ham_size}"
+
+
+  num = (spam / total_sample) 
+  denom = (total_word / total_sample)
+  percent_spammy = (num / denom) 
+
+  puts percent_spammy
+end
+
+calc_spamminess('free')
 
 
