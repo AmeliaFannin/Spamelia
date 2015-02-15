c0bb8ce6e962badecb4d867ef287fce9c5d4ab0a
Amelia Fannin <ameliafannin@gmail.com>
2015-02-15 09:41:58 -0500

    convert to sinatra

--- spamelia.rb
+++ spamelia.rb
@@ -1,7 +1,14 @@
+require 'sinatra'
+require 'bundler'
+
 require 'yaml'
 
+get '/' do
+  "Hi"
+end
+# multi_word_spam(test_message)
 
-def calc_spamminess(word)
+def probability_per_word(word)
   @ham = YAML::load_file "ham.yml"
   @spam = YAML::load_file "spam.yml"
 
@@ -38,7 +45,7 @@ def calc_spamminess(word)
     end
 end
 
-def multi_word_spam(message)
+def test_message(message)
   p_array = []
   stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
   likelyhood = 0
@@ -59,7 +66,7 @@ def multi_word_spam(message)
     next if w.length <= 3 || w.length > 15 || stopwords.include?(w)
     w.downcase!
     
-    pr = calc_spamminess(w)
+    pr = probability_per_word(w)
 
     pr = 0.99 if pr == 1.0
     p_array << pr   
@@ -79,25 +86,6 @@ def multi_word_spam(message)
   puts "message is #{likelyhood * 100}% likely spammy"
 end
 
-test_message = <<HEREDOC
-Martin Adamson wrote:
-> 
-> Isn't it just basically a mixture of beaten egg and bacon (or pancetta, 
-> really)? You mix in the raw egg to the cooked pasta and the heat of the pasta 
-> cooks the egg. That's my understanding.
-> 
 
-You're probably right, mine's just the same but with the cream added to the 
-eggs.  I guess I should try it without.  Actually looking on the internet for a 
-recipe I found this one from possibly one of the scariest people I've ever seen, 
-and he's a US Congressman:
-<http://www.virtualcities.com/ons/me/gov/megvjb1.htm>
 
-That's one of the worst non-smiles ever.
 
-Stew
-ps. Apologies if any of the list's Maine residents voted for this man, you won't 
-do it again once you've seen this pic.
-HEREDOC
-
-multi_word_spam(test_message)
